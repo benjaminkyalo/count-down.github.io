70e5add9f9a07cb2ef778a63a272331210c9670d
Benjamin Kyalo <kyalopaul120@gmail.co,m>
2023-12-27 22:32:10 +0300

    hope it works

---
+++ script.js
@@ -0,0 +1,29 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+// I am going to write this script and hope it functions.
+const Techmeeting = "1 jan 2024";
+
+function Countdown() {
+    const newYearsDate = new Date(Techmeeting); // Correct variable name and use 'new Date' instead of 'New Date'
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000; // Correct variable name
+
+    const days = Math.floor(totalSeconds / 3600 / 24); // Correct capitalization of 'Math'
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML =  days;
+    hoursEl.innerHTML = hours;
+    minutesEl.innerHTML = minutes;
+    secondsEl.innerHTML = seconds;
+}
+
+
+
+// Initial call
+setInterval(Countdown, 1000);
